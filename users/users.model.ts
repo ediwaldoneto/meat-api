b92f14fd487c045decc1a8c6aca33c35f8875632
Ediwaldo Neto <neto@F680V9.0>
2025-02-09 11:00:22 -0300

    rota por id

--- users/users.model.ts
+++ users/users.model.ts
@@ -3,13 +3,24 @@
  * The users array is hardcoded and will be replaced with a database implementation in the future.
  */
 const users = [
-  {name: 'João Cara de José', email: 'joao@emil.com'},
-  {name: 'Uoxiton Silva', email: 'uoxiton@email.com'},
+  {id:'1' , name: 'João Cara de José', email: 'joao@emil.com'},
+  {id:'2', name: 'Uoxiton Silva', email: 'uoxiton@email.com'},
 ];
 
 export class User {
 
   static findAll(): Promise<any[]> {
     return Promise.resolve(users);
+  }
+
+  static findById(id: string): Promise<any> {
+    return new Promise(resolve => {
+      const filtered = users.filter(user => user.id === id);
+      let user = undefined;
+      if (filtered.length > 0) {
+        user = filtered[0];
+      }
+      resolve(user);
+    });
   }
 }
